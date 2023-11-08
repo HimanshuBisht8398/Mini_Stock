a06e789b9436d0d056b06855bdf535171e542f4b
himanshubishtEM <himanshubisht@elitemindz.co>
2023-11-08 12:41:22 +0530

    Stock

---
+++ Backend/src/service/API.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const Stocks = require("../models/stock");
+const { GLOBAL_QUOTE_URL } = require("../constant");
+const API_KEY = process.env.API_KEY;
+
+exports.getQuotes = (symbol) => {
+  try {
+    const url = GLOBAL_QUOTE_URL.replace(
+      "{COMPANY_SYMBOL}",
+      `${symbol}.BSE`
+    ).replace("{API_KEY}", API_KEY);
+    return axios.get(url);
+  } catch (error) {
+    return {
+      msg: "Something went wrong",
+      error: error,
+    };
+  }
+};
+
+exports.getStocksFromDB = () => {
+  try {
+    return Stocks.find().select({ _id: 0, __v: 0 });
+  } catch (error) {
+    return {
+      msg: "Something went wrong",
+      error: error,
+    };
+  }
+};
+
+exports.generateRandomPrice = (low, high) => {
+  try {
+    return parseFloat((Math.random() * (high - low + 1)).toFixed(4)) + parseFloat(low);
+  } catch (error) {
+    return {
+      msg: "Something went wrong",
+      error: error,
+    };
+  }
+};
